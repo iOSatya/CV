b0e29e4b7a1a5482250a31990bf618de9add23bc
iOSatya <125645326+iOSatya@users.noreply.github.com>
2025-02-13 16:12:31 +0700

    Home Page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,11 +4,14 @@ import styles from "./Header.module.css";
 export default function Header() {
 
   return ( <> 
-  
-    <div className="fixed z-50 flex flex-row" style={{backdropFilter: 'blur(2px)'}}>
-      <NavLink to='/' className={({ isActive }) => [isActive && styles['active-link'], styles['nav-button']].join(' ')}>Home</NavLink>
-      <NavLink to='/about' className={({ isActive }) => [isActive && styles['active-link'], styles['nav-button']].join(' ')}>About</NavLink>
+
+    <div className="flex flex-row justify-center">
+      <div className="fixed z-50 flex flex-row w-4/5" style={{backdropFilter: 'blur(2px)'}}>
+        <NavLink to='/' className={({ isActive }) => [isActive && styles['active-link'], styles['nav-button']].join(' ')}>Home</NavLink>
+        <NavLink to='/about' className={({ isActive }) => [isActive && styles['active-link'], styles['nav-button']].join(' ')}>About</NavLink>
+      </div>
     </div>
+
   
   </> );
 
--- frontend/src/views/HomeView.jsx
+++ frontend/src/views/HomeView.jsx
@@ -1,9 +1,25 @@
+import { Link } from "react-router";
+import styles from "./HomeView.module.css";
+
 export default function HomeView() {
 
   return ( <> 
   
-    <div className="flex flex-col justify-center items-center" style={{height: "100vh"}}>
-      This is Home View
+    <div className="flex flex-row justify-center items-center h-screen">
+
+      <div className="w-4/5">
+        <div className="flex flex-col w-1/2">
+          <div className="mb-4">
+            <h1 className="font-bold text-3xl mb-2 slide-in">Wisanggeni Atthoriq Kuswirasatya</h1>
+            <p className="slide-in" style={{animationDelay: "0.2s"}}>I am a student with interest in web development and currently seeking for experience in this field.</p>
+          </div>
+
+          <div className="flex flex-row">
+            <Link className={["button", "slide-in", styles["button"]].join(" ")} style={{animationDelay: "0.4s"}}>Portofolio</Link>
+          </div>
+        </div>
+      </div>
+
     </div>
   
   </> );
